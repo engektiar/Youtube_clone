4be5da478190fa9cbc8516c294f9c6f0f19bb16e
engektiar <eng.ektiar@gmail.com>
2023-01-22 20:27:16 +0600

    videos app dwon

---
+++ index.js
@@ -0,0 +1,16 @@
+//  const mysidebar = document.getElementById("sidebar");
+// $(document).ready(function () {
+//   $("#btn").click(function () {
+//     $("#sidebar").toggle();   }); });
+
+function mainFunction() {
+  var element = document.getElementById("sidebar");
+   
+  element.classList.toggle("ektiar");
+  
+  
+}
+function myFunction() {
+  var element = document.getElementById("showMore");
+  element.classList.toggle("moreactive");
+}
